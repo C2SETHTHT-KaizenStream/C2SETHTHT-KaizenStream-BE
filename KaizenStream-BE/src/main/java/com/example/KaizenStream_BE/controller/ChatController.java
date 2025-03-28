316579b65b22f29ebea4c22ac8aadd980704b5ea
Hieu411 <huahieu.cv@gmail.com>
2025-03-28 14:52:26 +0700

    save chat to redis

--- KaizenStream-BE/src/main/java/com/example/KaizenStream_BE/controller/ChatController.java
+++ KaizenStream-BE/src/main/java/com/example/KaizenStream_BE/controller/ChatController.java
@@ -14,6 +14,61 @@ import org.springframework.stereotype.Controller;
 /**
  * The type Chat controller.
  */
+//@Controller
+//@RequiredArgsConstructor
+//@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+//public class ChatController {
+//
+//    ChatService chatService;
+//    SimpMessagingTemplate messagingTemplate;
+//
+//
+////    @MessageMapping("/chat/{livestreamId}")
+////    @SendTo("/topic/livestream/{livestreamId}")
+////    public ChatResponse sendMessage(@DestinationVariable String livestreamId, ChatResponse chatResponse) {
+////        chatResponse.setLivestreamId(livestreamId);
+////        return chatService.saveChatMessage(chatResponse);
+////    }
+//
+//    /**
+//     * Send message.
+//     *
+//     * @param livestreamId the livestream id
+//     * @param chatResponse the chat response
+//     */
+//    @MessageMapping("/chat/{livestreamId}")
+//    public void sendMessage(@DestinationVariable String livestreamId, ChatResponse chatResponse) {
+//        chatResponse.setLivestreamId(livestreamId);
+//        ChatResponse saved = chatService.saveChatMessage(chatResponse);
+//        messagingTemplate.convertAndSend("/topic/livestream/" + livestreamId, saved);
+//    }
+//
+//    @MessageMapping("/leaveMessage/{livestreamId}")
+//    public void leaveMessage(@DestinationVariable String livestreamId, String userName) {
+//        ChatResponse leaveMessage = new ChatResponse();
+//        leaveMessage.setMessage(userName + " has left the chat");
+//        leaveMessage.setUserId("SYSTEM");
+//        leaveMessage.setType("LEAVE");
+//        leaveMessage.setLivestreamId(livestreamId);
+//
+//        ChatResponse saved = chatService.saveChatMessage(leaveMessage); // Lưu vào database
+//        messagingTemplate.convertAndSend("/topic/livestream/" + livestreamId + "/message", saved);
+//    }
+//
+//    @MessageMapping("/joinMessage/{livestreamId}")
+//    public void joinMessage(@DestinationVariable String livestreamId, String userName) {
+//        ChatResponse joinMessage = new ChatResponse();
+//        joinMessage.setMessage(userName + " has joined the chat");
+//        joinMessage.setUserId("SYSTEM");
+//        joinMessage.setType("JOIN");
+//        joinMessage.setLivestreamId(livestreamId);
+//
+//        ChatResponse saved = chatService.saveChatMessage(joinMessage); // Lưu vào database
+//        messagingTemplate.convertAndSend("/topic/livestream/" + livestreamId + "/message", saved);
+//    }
+//}
+
+
 @Controller
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -22,20 +77,6 @@ public class ChatController {
     ChatService chatService;
     SimpMessagingTemplate messagingTemplate;
 
-
-//    @MessageMapping("/chat/{livestreamId}")
-//    @SendTo("/topic/livestream/{livestreamId}")
-//    public ChatResponse sendMessage(@DestinationVariable String livestreamId, ChatResponse chatResponse) {
-//        chatResponse.setLivestreamId(livestreamId);
-//        return chatService.saveChatMessage(chatResponse);
-//    }
-
-    /**
-     * Send message.
-     *
-     * @param livestreamId the livestream id
-     * @param chatResponse the chat response
-     */
     @MessageMapping("/chat/{livestreamId}")
     public void sendMessage(@DestinationVariable String livestreamId, ChatResponse chatResponse) {
         chatResponse.setLivestreamId(livestreamId);
@@ -43,33 +84,25 @@ public class ChatController {
         messagingTemplate.convertAndSend("/topic/livestream/" + livestreamId, saved);
     }
 
-    @MessageMapping("/leaveMessage/{livestreamId}")
-    public void leaveMessage(@DestinationVariable String livestreamId, String userName) {
-        ChatResponse leaveMessage = new ChatResponse();
-        leaveMessage.setMessage(userName + " has left the chat");
-        leaveMessage.setUserId("SYSTEM");
-        leaveMessage.setType("LEAVE");
-        leaveMessage.setLivestreamId(livestreamId);
-
-        ChatResponse saved = chatService.saveChatMessage(leaveMessage); // Lưu vào database
-        messagingTemplate.convertAndSend("/topic/livestream/" + livestreamId + "/message", saved);
-    }
-
-    @MessageMapping("/joinMessage/{livestreamId}")
+    @MessageMapping("/join/{livestreamId}")
     public void joinMessage(@DestinationVariable String livestreamId, String userName) {
         ChatResponse joinMessage = new ChatResponse();
         joinMessage.setMessage(userName + " has joined the chat");
         joinMessage.setUserId("SYSTEM");
         joinMessage.setType("JOIN");
         joinMessage.setLivestreamId(livestreamId);
-
-        ChatResponse saved = chatService.saveChatMessage(joinMessage); // Lưu vào database
-        messagingTemplate.convertAndSend("/topic/livestream/" + livestreamId + "/message", saved);
+        ChatResponse saved = chatService.saveChatMessage(joinMessage);
+        messagingTemplate.convertAndSend("/topic/livestream/" + livestreamId, saved);
     }
 
-
-
-
-
-
+    @MessageMapping("/leave/{livestreamId}")
+    public void leaveMessage(@DestinationVariable String livestreamId, String userName) {
+        ChatResponse leaveMessage = new ChatResponse();
+        leaveMessage.setMessage(userName + " has left the chat");
+        leaveMessage.setUserId("SYSTEM");
+        leaveMessage.setType("LEAVE");
+        leaveMessage.setLivestreamId(livestreamId);
+        ChatResponse saved = chatService.saveChatMessage(leaveMessage);
+        messagingTemplate.convertAndSend("/topic/livestream/" + livestreamId, saved);
+    }
 }
