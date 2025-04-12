79a2c71e5c6e65c27b51d7226654fcc3aba22e0f
TUDG <tranduongtruong1623@gmail.com>
2025-04-12 15:33:00 +0700

    Fix rewatch

--- KaizenStream-BE/src/main/java/com/example/KaizenStream_BE/controller/LiveStreamController.java
+++ KaizenStream-BE/src/main/java/com/example/KaizenStream_BE/controller/LiveStreamController.java
@@ -24,12 +24,9 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.InputStreamReader;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -44,7 +41,7 @@ public class LiveStreamController {
     LivestreamService livestreamService;
     @Autowired
     LivestreamMapper livestreamMapper;
-   // private final Map<String, Process> syncProcesses = new HashMap<>();
+    // private final Map<String, Process> syncProcesses = new HashMap<>();
     private static Process syncProcess = null; // Chỉ có một tiến trình đồng bộ HLS
     private static  final AtomicInteger activeStreams = new AtomicInteger(0); // Đếm số luồng đang stream
 
@@ -75,7 +72,7 @@ public class LiveStreamController {
 
     @GetMapping("/{id}")
     ApiResponse<LivestreamRespone>  getLivestreamById(@PathVariable("id") String id){
-         return ApiResponse.<LivestreamRespone>builder().result(livestreamService.getLivestreamById(id)).code(200).build();
+        return ApiResponse.<LivestreamRespone>builder().result(livestreamService.getLivestreamById(id)).code(200).build();
     }
     @PutMapping()
     ApiResponse<LivestreamRespone> updateById(@RequestBody @Valid UpdateLivestreamRequest updateLivestreamRequest){
@@ -110,43 +107,43 @@ public class LiveStreamController {
 
         if (activeStreamCount == 1 && syncProcess == null) {
 
-        try {
-            ProcessBuilder pb = new ProcessBuilder("powershell", "-ExecutionPolicy", "Bypass", "-File",
-                    syncHlsUrl, processName);
-            syncProcess = pb.start(); // Khởi tạo tiến trình đồng bộ
-            System.out.println("✅ Script đồng bộ HLS đang chạy trong nền ");
-        } catch (IOException e) {
-            System.err.println("❌ Lỗi khi chạy PowerShell script: " + e.getMessage());
-            return ApiResponse.<String>builder().result("Failed to start sync script").code(500).build();
-        }
+            try {
+                ProcessBuilder pb = new ProcessBuilder("powershell", "-ExecutionPolicy", "Bypass", "-File",
+                        syncHlsUrl, processName);
+                syncProcess = pb.start(); // Khởi tạo tiến trình đồng bộ
+                System.out.println("✅ Script đồng bộ HLS đang chạy trong nền ");
+            } catch (IOException e) {
+                System.err.println("❌ Lỗi khi chạy PowerShell script: " + e.getMessage());
+                return ApiResponse.<String>builder().result("Failed to start sync script").code(500).build();
+            }
         }
         return ApiResponse.<String>builder().result("Start new livestream").code(200).build();
     }
 
-//    @PostMapping("/end")
-//    public ResponseEntity<String> endStream(@RequestParam String name) throws InterruptedException {
-//        String streamKey=getKey(name);
-//        System.out.println("🛑 Dừng stream với streamKey: " + streamKey);
-//        int activeStreamCount = activeStreams.decrementAndGet();
-//        // Nếu không còn luồng nào, dừng tiến trình đồng bộ
-//        if (activeStreamCount == 0 && syncProcess != null) {
-//            // Chờ 10 giây trước khi dừng tiến trình
-//            try {
-//                System.out.println("⏳ Đợi 7 giây trước khi dừng tiến trình...");
-//                Thread.sleep(5000); // Chờ 10 giây (10,000 milliseconds)
-//                stopSyncProcess();
-//
-//            } catch (InterruptedException e) {
-//                System.err.println("❌ Lỗi khi chờ trước khi dừng tiến trình: " + e.getMessage());
-//                return ResponseEntity.status(500).body("Error while waiting to stop stream");
-//            }
-//        }
-//        generateM3u8File(streamKey);
-//        //Thread.sleep(5000); // Chờ 10 giây (10,000 milliseconds)
-//       // livestreamService.updateStatus(streamKey, Status.ENDED);
-//
-//        return ResponseEntity.ok("Stream ended");
-//    }
+    @PostMapping("/end")
+    public ResponseEntity<String> endStream(@RequestParam String name) throws InterruptedException {
+        String streamKey=getKey(name);
+        System.out.println("🛑 Dừng stream với streamKey: " + streamKey);
+        int activeStreamCount = activeStreams.decrementAndGet();
+        // Nếu không còn luồng nào, dừng tiến trình đồng bộ
+        if (activeStreamCount == 0 && syncProcess != null) {
+            // Chờ 10 giây trước khi dừng tiến trình
+            try {
+                System.out.println("⏳ Đợi 7 giây trước khi dừng tiến trình...");
+                Thread.sleep(10000); // Chờ 10 giây (10,000 milliseconds)
+                stopSyncProcess();
+
+            } catch (InterruptedException e) {
+                System.err.println("❌ Lỗi khi chờ trước khi dừng tiến trình: " + e.getMessage());
+                return ResponseEntity.status(500).body("Error while waiting to stop stream");
+            }
+        }
+        generateM3u8File(streamKey);
+        //Thread.sleep(5000); // Chờ 10 giây (10,000 milliseconds)
+        // livestreamService.updateStatus(streamKey, Status.ENDED);
+
+        return ResponseEntity.ok("Stream ended");
+    }
 
     private void stopSyncProcess() {
         if (syncProcess != null && syncProcess.isAlive()) {
@@ -172,12 +169,7 @@ public class LiveStreamController {
                 return ResponseEntity.badRequest().body("Không tìm thấy file .ts");
             }
 
-            String m3u8Content = generateM3u8Content(tsFiles);
-
-            // Ghi ra local để FFmpeg dùng
-            String localM3u8Path = "temp/hls/" + streamId + "/playlist.m3u8";
-            Files.write(Paths.get(localM3u8Path), m3u8Content.getBytes(StandardCharsets.UTF_8));
-            System.out.println("✅ Đã tạo file local playlist.m3u8 tại " + localM3u8Path);
+            String m3u8Content = generateM3u8Content(tsFiles,streamId);
             minioService.uploadM3u8ToMinIO(streamId, m3u8Content);
             //Thread.sleep(7000); // Chờ 10 giây (10,000 milliseconds)
 
@@ -191,7 +183,7 @@ public class LiveStreamController {
         }
     }
 
-    private String generateM3u8Content(List<String> tsFileNames) {
+    private String generateM3u8Content(List<String> tsFileNames, String streamId) {
         StringBuilder sb = new StringBuilder();
         sb.append("#EXTM3U\n");
         sb.append("#EXT-X-VERSION:3\n");
@@ -199,18 +191,22 @@ public class LiveStreamController {
         sb.append("#EXT-X-MEDIA-SEQUENCE:0\n");
 
         for (String ts : tsFileNames) {
-            sb.append("#EXTINF:10.0,\n");
-            sb.append(ts + ".ts\n");
+            sb.append("#EXTINF:2.000,\n");
+            String fileNameOnly = ts.substring(ts.lastIndexOf("/") + 1);
+            sb.append( fileNameOnly+ ".ts\n");
         }
+
+
 
         sb.append("#EXT-X-ENDLIST\n");
         return sb.toString();
     }
+
+
     private  String getKey(String name){
         if(!name.contains(",")) return name;
         return name.substring(name.lastIndexOf(",")+1,name.length());
     }
-
     @GetMapping("/{streamId}/playlist-url")
     public ResponseEntity<String> getM3u8Url(@PathVariable String streamId) {
         try {
@@ -221,101 +217,4 @@ public class LiveStreamController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Lỗi: " + e.getMessage());
         }
     }
-
-
-
-
-
-
-    public void generateMultiQualityHLS(String streamId) throws IOException, InterruptedException {
-        String inputM3u8 = "temp/hls/" + streamId + "/playlist.m3u8"; // giả sử bạn đã tạo playlist.m3u8
-        String outputDir = "temp/hls/" + streamId;
-
-        ProcessBuilder pb = new ProcessBuilder(
-                "ffmpeg",
-                "-analyzeduration", "2147483647",
-                "-probesize", "2147483647", "-i", inputM3u8,
-                "-filter_complex", "[0:v]split=3[v1][v2][v3];" +
-                "[v1]scale=1920:1080[vout1];" +
-                "[v2]scale=1280:720[vout2];" +
-                "[v3]scale=854:480[vout3]",
-                // 1080p
-                "-map", "[vout1]", "-map", "a", "-c:v:0", "libx264", "-b:v:0", "5000k", "-c:a:0", "aac",
-                "-f", "hls", "-hls_time", "4", "-hls_playlist_type", "vod",
-                "-hls_segment_filename", outputDir + "/1080p_%03d.ts", outputDir + "/1080p.m3u8",
-                // 720p
-                "-map", "[vout2]", "-map", "a", "-c:v:1", "libx264", "-b:v:1", "3000k", "-c:a:1", "aac",
-                "-f", "hls", "-hls_time", "4", "-hls_playlist_type", "vod",
-                "-hls_segment_filename", outputDir + "/720p_%03d.ts", outputDir + "/720p.m3u8",
-                // 480p
-                "-map", "[vout3]", "-map", "a", "-c:v:2", "libx264", "-b:v:2", "1500k", "-c:a:2", "aac",
-                "-f", "hls", "-hls_time", "4", "-hls_playlist_type", "vod",
-                "-hls_segment_filename", outputDir + "/480p_%03d.ts", outputDir + "/480p.m3u8"
-        );
-
-        pb.inheritIO(); // log output ra console
-        Process process = pb.start();
-        process.waitFor();
-    }
-    public void generateMasterM3u8(String streamId) throws IOException {
-        String masterContent = """
-        #EXTM3U
-        #EXT-X-STREAM-INF:BANDWIDTH=5000000,RESOLUTION=1920x1080
-        1080p.m3u8
-        #EXT-X-STREAM-INF:BANDWIDTH=3000000,RESOLUTION=1280x720
-        720p.m3u8
-        #EXT-X-STREAM-INF:BANDWIDTH=1500000,RESOLUTION=854x480
-        480p.m3u8
-        """;
-
-        Path masterPath = Paths.get("temp/hls/" + streamId + "/master.m3u8");
-        Files.write(masterPath, masterContent.getBytes());
-    }
-    @PostMapping("/end")
-    public ApiResponse<String> endStream(@RequestParam String name) {
-        String streamKey = getKey(name);
-        System.out.println("🛑 Dừng stream với streamKey: " + streamKey);
-
-        try {
-            System.out.println("📥 Đang tải .ts từ MinIO...");
-            minioService.downloadHlsToLocal(streamKey);
-            Thread.sleep(10000);
-            System.out.println("🧩 Tạo playlist.m3u8 từ .ts...");
-            generateM3u8File(streamKey);
-            Thread.sleep(10000);
-            System.out.println("🎞️ Tái xử lý sang 1080p/720p/480p...");
-            generateMultiQualityHLS(streamKey);
-            Thread.sleep(10000);
-
-            System.out.println("📝 Tạo master.m3u8...");
-            generateMasterM3u8(streamKey);
-
-            System.out.println("☁️ Upload lại lên MinIO...");
-            minioService.uploadMultiQualityHLS(streamKey);
-
-            deleteLocalTempDirectory(streamKey);
-
-            return ApiResponse.<String>builder().result("✅ Xử lý multi-quality HLS hoàn tất").build();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return  ApiResponse.<String>builder().result("❌ Lỗi xử lý: " + e.getMessage()).build();
-        }
-    }
-
-
-    public void deleteLocalTempDirectory(String streamId) throws IOException {
-        Path dir = Paths.get("temp/hls/" + streamId);
-        if (Files.exists(dir)) {
-            Files.walk(dir)
-                    .sorted(Comparator.reverseOrder())
-                    .map(Path::toFile)
-                    .forEach(File::delete);
-            System.out.println("🧹 Đã dọn thư mục tạm temp/hls/" + streamId);
-        }
-    }
-
-
-
-
-
 }
--- KaizenStream-BE/src/main/java/com/example/KaizenStream_BE/service/MinioService.java
+++ KaizenStream-BE/src/main/java/com/example/KaizenStream_BE/service/MinioService.java
@@ -3,25 +3,25 @@ package com.example.KaizenStream_BE.service;
 import io.minio.*;
 import io.minio.http.Method;
 import io.minio.messages.Item;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 
+@Slf4j
 @Service
 public class MinioService {
     private final MinioClient minioClient;
@@ -48,16 +48,16 @@ public class MinioService {
                 .contentType("application/x-mpegURL")
                 .build());
     }
-//    public String getPresignedM3u8Url(String streamId, int expireInSeconds) throws Exception {
-//        String objectName = "hls/" + streamId + "/playlist.m3u8";
-//
-//        return minioClient.getPresignedObjectUrl(GetPresignedObjectUrlArgs.builder()
-//                .method(Method.GET)
-//                .bucket(bucketName)
-//                .object(objectName)
-//                .expiry(expireInSeconds)
-//                .build());
-//    }
+    public String getPresignedM3u8Url(String streamId, int expireInSeconds) throws Exception {
+        String objectName = "hls/" + streamId + "/playlist.m3u8";
+
+        return minioClient.getPresignedObjectUrl(GetPresignedObjectUrlArgs.builder()
+                .method(Method.GET)
+                .bucket(bucketName)
+                .object(objectName)
+                .expiry(expireInSeconds)
+                .build());
+    }
 
     public List<String> listTsFiles(String streamId) throws Exception {
         List<String> segmentFiles = new ArrayList<>();
@@ -92,70 +92,57 @@ public class MinioService {
         segmentFiles.forEach(s -> System.out.println(s));
         return segmentFiles;
     }
+//    public double getTsDurationFromMinio(String streamId, String ts) {
+//        try {
+//            // Thư mục tạm trong project
+//            String tempDir = System.getProperty("user.dir") + File.separator + "temp_ts";
+//            Files.createDirectories(Path.of(tempDir)); // Tạo thư mục nếu chưa có
+//
+//            // File tạm theo tên ts (ví dụ: 3.ts → temp_ts/3.ts)
+//            Path tempFile = Path.of(tempDir, ts + ".ts");
+//
+//            // Tải file từ MinIO về thư mục tạm
+//            minioClient.downloadObject(DownloadObjectArgs.builder()
+//                    .bucket(bucketName)
+//                    .object("hls/" + streamId + "/" + ts + ".ts")
+//                    .filename(tempFile.toString())
+//                    .build());
+//
+//            // Đo thời lượng
+//            double duration = getTsDurationUsingFFmpeg(tempFile.toString());
+//
+//            // Xoá file sau khi đo xong
+//           // Files.deleteIfExists(tempFile);
+//
+//            return duration;
+//        } catch (Exception e) {
+//            System.err.println("❌ Lỗi đo thời lượng ts: " + e.getMessage());
+//            return 2.0; // fallback
+//        }
+//    }
+//
+//
+//    private double getTsDurationUsingFFmpeg(String filePath) {
+//        log.warn("File line: "+filePath);
+//        try {
+//            ProcessBuilder pb = new ProcessBuilder(
+//                    "ffmpeg", "-i", filePath,
+//                    "-v", "error", "-show_entries", "format=duration",
+//                    "-of", "default=noprint_wrappers=1:nokey=1"
+//            );
+//            Process process = pb.start();
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+//            String line = reader.readLine();
+//            process.waitFor();
+//            log.warn("line "+line);
+//            return Double.parseDouble(line); // Trả về duration
+//        } catch (Exception e) {
+//            System.err.println("❌ Lỗi đo thời gian bằng FFmpeg: " + e.getMessage());
+//            return 2.0; // fallback
+//        }
+//    }
 
 
 
-
-
-    public void downloadHlsToLocal(String streamId) throws Exception {
-        String prefix = "hls/" + streamId + "/";
-        String basePath = System.getProperty("user.dir");
-        String localPath = basePath + "/temp/hls/" + streamId + "/";
-        Files.createDirectories(Paths.get(localPath));
-
-        Iterable<Result<Item>> items = minioClient.listObjects(ListObjectsArgs.builder()
-                .bucket(bucketName)
-                .prefix(prefix)
-                .build());
-
-        for (Result<Item> result : items) {
-            String objectName = result.get().objectName();
-            if (objectName.endsWith(".ts")) {
-                InputStream stream = minioClient.getObject(GetObjectArgs.builder()
-                        .bucket(bucketName)
-                        .object(objectName)
-                        .build());
-
-                // Lấy tên file cuối cùng
-                String fileName = objectName.substring(objectName.lastIndexOf("/") + 1);
-                Path localFilePath = Paths.get(localPath, fileName);
-                System.out.println("downloadHlsToLocal\n"+ fileName);
-                System.out.println("localFilePath\n"+ localFilePath);
-
-                Files.copy(stream, localFilePath, StandardCopyOption.REPLACE_EXISTING);
-                stream.close();
-            }
-        }
-    }
-
-    public void uploadMultiQualityHLS(String streamId) throws Exception {
-        String dir = "temp/hls/" + streamId + "/";
-        File[] files = new File(dir).listFiles();
-        if (files != null) {
-            for (File file : files) {
-                if (file.isFile()) {
-                    try (InputStream is = new FileInputStream(file)) {
-                        minioClient.putObject(PutObjectArgs.builder()
-                                .bucket(bucketName)
-                                .object("hls/" + streamId + "/" + file.getName())
-                                .stream(is, file.length(), -1)
-                                .contentType(file.getName().endsWith(".m3u8") ? "application/x-mpegURL" : "video/MP2T")
-                                .build());
-                    }
-                }
-            }
-        }
-    }
-
-    public String getPresignedM3u8Url(String streamId, int expireSeconds) throws Exception {
-        return minioClient.getPresignedObjectUrl(
-                GetPresignedObjectUrlArgs.builder()
-                        .bucket(bucketName)
-                        .object("hls/" + streamId + "/master.m3u8")
-                        .method(Method.GET)
-                        .expiry(expireSeconds)
-                        .build()
-        );
-    }
 
 }
