c7abc751fce0e091ac2daa0f39c7c5decbf3f8ae
vatuan <anhtuan01012003@gmail.com>
2025-04-21 15:50:19 +0700

    Add field bankHolder in Withdraw entity, Update bankHolder in Withdraw function, delete commented code in PaymentService

--- KaizenStream-BE/src/main/java/com/example/KaizenStream_BE/service/PaymentService.java
+++ KaizenStream-BE/src/main/java/com/example/KaizenStream_BE/service/PaymentService.java
@@ -57,39 +57,6 @@ public class PaymentService {
         walletRepository.save(wallet); // Gọi phương thức `save` của `WalletRepository`
     }
 
-//    public void handlePaymentSuccess(String sessionId, String userId, double amount) {
-//        // Tìm user theo userId
-//        User user = userRepository.findById(userId)
-//                .orElseThrow(() -> new RuntimeException("User not found"));
-//
-//        // Tính số điểm: 10 USD = 100 điểm
-//        // Lấy số tiền thanh toán từ tham số `amount` và tính số điểm tương ứng
-//        int points = (int) (amount * 10); // Mỗi 10 USD = 100 điểm
-//
-//        // Tạo đối tượng Purchase và gán giá trị
-//        Purchase purchase = new Purchase();
-//        purchase.setUser(user); // Đảm bảo `user` không phải null
-//        purchase.setAmount(amount); // Sử dụng số tiền thanh toán
-//        purchase.setPointReceived(points); // Lưu số điểm vào bảng `purchases`
-//        purchase.setPurchaseDate(LocalDateTime.now());  // Đảm bảo có ngày giao dịch
-//
-//        // Lưu thông tin vào bảng `purchases`
-//        purchaseRepository.save(purchase); // Gọi phương thức `save` của `PurchaseRepository`
-//
-//        // Cập nhật số dư ví
-//        Wallet wallet = walletRepository.findByUser(user).orElse(null);
-//        if (wallet == null) {
-//            wallet = new Wallet();
-//            wallet.setUser(user);
-//            wallet.setBalance(points); // Tạo mới ví và gán điểm vào
-//        } else {
-//            wallet.setBalance(wallet.getBalance() + points); // Cộng điểm vào ví
-//        }
-//
-//        // Lưu cập nhật ví vào cơ sở dữ liệu
-//        walletRepository.save(wallet); // Gọi phương thức `save` của `WalletRepository`
-//    }
-
     // Phương thức để lấy lịch sử mua điểm của một user
     public ApiResponse<List<PurchaseResponse>> getPurchaseHistory(String userId) {
         // Tìm user theo userId
